a569c35e1870d348d72e6687e732468748b01c53
Jesper Shu-Hao Annebäck <anneback@csc.kth.se>
2013-02-25 17:22:05 +0100

    syntax fixes

--- view/dragAndDropListView.js
+++ view/dragAndDropListView.js
@@ -5,20 +5,20 @@ function DragAndDropListView (model) {
 	function setupListView(){
 		var tmpList = model.days;
 		for (var i = model.days.length - 1; i >= 0; i--) {
-			var activity = tmpList.[i];
+			var activity = tmpList[i];
 			var tr = $('<tr>');
 			//[0]presentation = .info, [1]discussion = .success, [2]break = .warning, [3]groupwork = .error
 			//"Presentation" = info,"Group Work" = .error,"Discussion" = .success,"Break" = .warning
-			if(activity.getType.equals("Presentation")) {
+			if(activity.getType().equals("Presentation")) {
 				tr.attr('class','info');
 			} else 
-			if(activity.getType.equals("Group Work")) {
+			if(activity.getType().equals("Group Work")) {
 				tr.attr('class','error');
 			} else
-			if(activity.getType.equals("Discussion")) {
+			if(activity.getType().equals("Discussion")) {
 				tr.attr('class','success');
 			} else
-			if(activity.getType.equals("Break")) {
+			if(activity.getType().equals("Break")) {
 				tr.attr('class','warning');
 			}
 			tr.append('<td>'+activity._length+'</td>');
