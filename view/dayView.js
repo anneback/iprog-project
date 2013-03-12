dd514a2e056ee6268701075aa27a812e10e15b36
eppi <eppi@eppi-lubuntu.(none)>
2013-03-12 13:50:03 +0100

    tried to fix timepicker and added hardcoded progressbar-interface

--- view/dayView.js
+++ view/dayView.js
@@ -7,12 +7,15 @@ function DayView(model) {
 	var startTime = '08:00';
 	var endTime = '12:00';
 	var totalLength = 20;
+	var dayNumber = 1;
+	var totalLengthDiscussion = 14;
+	var totalLengthGroupWork = 32;
+	var totalLengthPresentation = 36;
+	var totalLengthBreak = 18;
 
 	setupTable();
 
 	function setupTable() {
-
-		var dayNumber = 1;
 
 		var dayDiv =  $('<div>');
 		dayDiv.attr('id','day'+dayNumber);
@@ -20,27 +23,48 @@ function DayView(model) {
 
 		var timePickerDiv = $('<div>');
 		timePickerDiv.attr('class','input-append bootstrap-timepicker');
-		timePickerDiv.append('<input id="startTime" type="text" class="input-small">');
+		timePickerDiv.append('<input id="startTime'+dayNumber+'" type="text" class="input-small">');
 		timePickerDiv.append('<span class="add-on"><i class="icon-time"></i></span>');
 		dayDiv.append(timePickerDiv);
 
 		var endTimeDiv = $('<div>');
-		endTimeDiv.attr('id','endTime');
+		endTimeDiv.attr('id','endTime'+dayNumber);
 		endTimeDiv.attr('style','text-align: center');
 		endTimeDiv.append('End time: '+endTime);
 		dayDiv.append(endTimeDiv);
 
-		var totalLength = $('<div>');
-		totalLength.attr('id','endTime');
-		totalLength.attr('style','text-align: center');
-		totalLength.append('Total length: '+totalLength);
-		dayDiv.append(totalLength);
+		var totalLengthDiv = $('<div>');
+		totalLengthDiv.attr('id','totalLength'+dayNumber);
+		totalLengthDiv.attr('style','text-align: center');
+		totalLengthDiv.append('Total length: '+totalLength);
+		dayDiv.append(totalLengthDiv);
+
+		var progressBarDiscussionDiv = $('<div>');
+		progressBarDiscussionDiv.attr('class','bar bar-success');
+		progressBarDiscussionDiv.attr('style','width: '+totalLengthDiscussion+'%');
+		var progressBarPresentationDiv = $('<div>');
+		progressBarPresentationDiv.attr('class','bar bar-info');
+		progressBarPresentationDiv.attr('style','width: '+totalLengthPresentation+'%');
+		var progressBarBreakDiv = $('<div>');
+		progressBarBreakDiv.attr('class','bar bar-warning');
+		progressBarBreakDiv.attr('style','width: '+totalLengthBreak+'%');
+		var progressBarGroupWorkDiv = $('<div>');
+		progressBarGroupWorkDiv.attr('class','bar bar-danger');
+		progressBarGroupWorkDiv.attr('style','width: '+totalLengthGroupWork+'%');
+
+		var progressBarDiv = $('<div>');
+		progressBarDiv.attr('id','progressBar'+dayNumber);
+		progressBarDiv.attr('class','progress');
+		progressBarDiv.append(progressBarDiscussionDiv);
+		progressBarDiv.append(progressBarPresentationDiv);
+		progressBarDiv.append(progressBarBreakDiv);
+		progressBarDiv.append(progressBarGroupWorkDiv);
+		dayDiv.append(progressBarDiv);
 
 		var tr = $('<tr>');
-		tr.append('<td>intro2</td>');
 
 		var dayTable = $('<table>');
-		dayTable.attr('id','scheduleTable');
+		dayTable.attr('id','scheduleTable'+dayNumber);
 		dayTable.attr('class','table table-bordered');
 		dayTable.append(tr);
 
@@ -51,5 +75,9 @@ function DayView(model) {
 		dayDiv.append(dndContainerDiv);
 
 		$('#dayCanvas').append(dayDiv);
+	}
+
+	function incrementDayNumber() {
+		dayNumber++;
 	}
 }
--- view/dayViewController.js
+++ view/dayViewController.js
@@ -3,9 +3,10 @@ function DayViewController(model) {
 	this.model = model;
 
 	$("#addADayButton").click(
-		function(){	
+		function(){
 			model.addDay();
 			var dayView = new DayView(model);
+			//dayView.incrementDayNumber();
 		});
 
 }
